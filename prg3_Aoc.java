7324e9609178701b99f74259bfc9d5760913167a
ankitke <ankitkumarkeshrifbg8543@gmail.com>
2023-10-20 13:06:49 +0530

    new file added working new branch

---
+++ prg3_Aoc.java
@@ -0,0 +1,10 @@
+class Arc
+{
+	public static void main(String arg[])
+	{
+		float pi=3.14f, aoc;
+		int r=5;
+		aoc=pi*r*r;
+		System.out.println("Area of Circle is = "+aoc);
+	}
+}
---
+++ prg4_Div.java
@@ -0,0 +1,11 @@
+class Division
+{
+public static void main(String []args)
+{
+int a=10,b=3;
+float div;
+div=(float)a/b;
+System.out.println("Division of "+ a + "and" + b +" ="+div);
+}
+}
+
