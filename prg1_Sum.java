320acb524f7b5355a63420a773ecf861cb715dcd
ankitke <ankitkumarkeshrifbg8543@gmail.com>
2023-10-20 11:53:56 +0530

    first commit

---
+++ prg1_Sum.java
@@ -0,0 +1,18 @@
+import javax.swing.JOptionPane;
+class MyCl
+{
+	public static void main(String []arg)
+	{
+		int a,b,r;
+		String s1,s2;
+		s1=JOptionPane.showInputDialog("Enter No");
+		s2=JOptionPane.showInputDialog("Enter No");
+		
+		a=Integer.parseInt(s1);
+		b=Integer.parseInt(s2);
+		r=a+b;
+		JOptionPane.showMessageDialog(null,"Sum of "+a+"and "+b+"is: "+r);
+		//System.out.println("Sum of "+a+"and "+b+"is: "+r);
+		
+	}
+}
---
+++ prg2_Sub.java
@@ -0,0 +1,8 @@
+class Keshri
+{
+public static void main(String arg[])
+{
+int a=20,b=10;
+System.out.println(a-b);
+}
+}
